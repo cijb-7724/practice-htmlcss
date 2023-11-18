750c5cedb8bf8c60f9f73939f415d2dc17b4c377
cijb-7724 <johokamia0@gmail.com>
2023-11-19 05:16:25 +0900

    dom dom

--- scr.js
+++ scr.js
@@ -9,3 +9,15 @@ for (var i=0; i<btns.length; ++i) {
         this.classList.toggle('clicked');
     });
 }
+
+
+var btnTs = document.getElementsByClassName('btnT');
+for (var i=0; i<btnTs.length; ++i) {
+    btnTs[i].addEventListener('click', function() {
+        this.innerHTML = 'クリックすな';
+        for (var j=0; j<btnTs.length; ++j) {
+            btnTs[j].classList.remove('clickedT');
+        }
+        this.classList.toggle('clickedT');
+    });
+}
